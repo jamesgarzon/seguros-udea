41ff2eb82b929c14075e9862743045bba64805cd
Live session user <jamesgarzon92@gmail.com>
2015-06-13 21:48:50 +0000

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name segurosUdeaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the segurosUdeaApp
+ */
+angular.module('segurosUdeaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
